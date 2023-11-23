7fce838e0f5311c54d02b21a896dcb676329a55d
nikhilyadav2006 <151811522+nikhilyadav2006@users.noreply.github.com>
2023-11-23 11:57:10 +0530

    Add files via upload

---
+++ a.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float r,a;
+    printf("enter the radius of circle\n");
+    scanf("%f", &r);
+    a=3.14*r*r;
+    printf("Area of circleis:%f",a);
+    return 0;
+}
