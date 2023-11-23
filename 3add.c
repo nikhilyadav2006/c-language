f2475733e6408415b3a86fc5d4c4dc911caaf72a
nikhilyadav2006 <151811522+nikhilyadav2006@users.noreply.github.com>
2023-11-23 11:57:49 +0530

    Add files via upload

---
+++ 3add.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b,c,d;
+    printf("enter three number");
+    scanf("%d %d %d",&a,&b,&c);
+    d=a+b+c;
+    printf("%d+%d+%d=%d" ,a,b,c,d);
+    return 0;
+}
+
+
+    
+ 
