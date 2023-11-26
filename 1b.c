6e6814183498c8fcb76e190e5b2d2530c04068fc
nikhilyadav2006 <151811522+nikhilyadav2006@users.noreply.github.com>
2023-11-26 13:41:32 +0530

    Add files via upload

---
+++ 1b.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, d;
+    printf("Enter Three no.\n");
+    scanf("%d%d%d", &a, &b, &c);
+    d=a+b+c;
+    printf("%d+%d+%d=%d", a, b, c, d);
+    return 0;
+}
